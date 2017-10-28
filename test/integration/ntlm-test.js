8f621a36855ce6e9e5c7c30e7359eae65c3e33d1
Arthur Schreiber <arthurschreiber@github.com>
2017-10-28 09:48:42 +0200

    Disable integration tests for now.

--- test/integration/ntlm-test.js
+++ test/integration/ntlm-test.js
@@ -1,32 +1,32 @@
 // @flow
 
-const { Connection } = require('tedious');
-const ntlmAuthProvider = require('../../src');
-
-describe('Authenticating to SQLServer via NTLM', function() {
-  let workGroupConfig;
-
-  beforeEach(function() {
-    workGroupConfig = {
-      server: process.env.TDS_WORKGROUP_SERVER,
-
-      authProvider: ntlmAuthProvider({
-        domain: process.env.TDS_WORKGROUP_NAME,
-        username: process.env.TDS_WORKGROUP_USERNAME,
-        password: process.env.TDS_WORKGROUP_PASSWORD
-      })
-    };
-  });
-
-  it('works', function(done) {
-    const connection = new Connection(workGroupConfig);
-    connection.on('connect', function(err) {
-      if (err) {
-        return done(err);
-      }
-
-      connection.on('end', done);
-      connection.close();
-    });
-  });
-});
+// const { Connection } = require('tedious');
+// const ntlmAuthProvider = require('../../src');
+//
+// describe('Authenticating to SQLServer via NTLM', function() {
+//   let workGroupConfig;
+//
+//   beforeEach(function() {
+//     workGroupConfig = {
+//       server: process.env.TDS_WORKGROUP_SERVER,
+//
+//       authProvider: ntlmAuthProvider({
+//         domain: process.env.TDS_WORKGROUP_NAME,
+//         username: process.env.TDS_WORKGROUP_USERNAME,
+//         password: process.env.TDS_WORKGROUP_PASSWORD
+//       })
+//     };
+//   });
+//
+//   it('works', function(done) {
+//     const connection = new Connection(workGroupConfig);
+//     connection.on('connect', function(err) {
+//       if (err) {
+//         return done(err);
+//       }
+//
+//       connection.on('end', done);
+//       connection.close();
+//     });
+//   });
+// });
